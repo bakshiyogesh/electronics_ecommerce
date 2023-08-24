a3ec228986142c39696a18f5e4eb3d565df95568
Zestgeek-22 <zestgeek-22@Zestgeek-22s-MacBook-Air.local>
2023-08-24 18:44:13 +0530

    image for background added.

--- src/components/common/ProductContainer.tsx
+++ src/components/common/ProductContainer.tsx
@@ -1,26 +1,43 @@
 import { FC } from "react";
-import Container from '@mui/material/Container';
-import { Box } from "@mui/material";
-const ProductContainer:FC=()=>{
-    const imageURL='/pexels-lukas-1420702.jpeg'
-    return(
-<>
-<Container fixed maxWidth='lg'>
-        {/* < sx={{ bgcolor: '#cfe8fc', height: '100vh' }} /> */}
-        <Box
-      component="div"
-      sx={{
-        position: 'absolute',
-        width: '100%',
-        height: '100%',
-        backgroundImage: `url(${imageURL})`,
-        backgroundPosition: 'center',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-      }}
-    />
-      </Container>
-</>
-    )
-}
+import Container from "@mui/material/Container";
+import { Box, CardMedia, Paper, Grid } from "@mui/material";
+// import CardMedia from '@mui/material/CardMedia';
+const ProductContainer: FC = () => {
+  const imageURL = "/pexels-lukas-1420702.jpeg";
+
+  return (
+    <>
+      {/* <Box
+        sx={{
+          position: "absolute",
+          width: "100vw",
+          height: "100%",
+          backgroundImage: `url(${imageURL})`,
+        //   backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          mt:2
+        }}
+      /> */}
+
+<Grid container >
+  <Grid item xs={12}>
+  <Box
+        sx={{
+          position: "absolute",
+          width: "100vw",
+          height: "100%",
+          backgroundImage: `url(${imageURL})`,
+        //   backgroundPosition: "center",
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          mt:2
+        }}
+      />
+  </Grid>
+</Grid>
+
+    </>
+  );
+};
 export default ProductContainer;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,75 @@
 import { FC } from "react";
 import NavBar from "../components/common/Navbar";
 import ProductContainer from "../components/common/ProductContainer";
+import ProductCard from "../components/common/Card";
+import { Grid, Container, Stack } from "@mui/material";
+// import "./style.css";
+
 
 const HomePage:FC=()=>{
+  const imageURL = "/pexels-lukas-1420702.jpeg";
+
  return(
 <>
-<NavBar/>
-<ProductContainer/>
+    <NavBar/>
+
+
+    {/* <div style={{ display: 'flex', flexDirection: 'column', marginTop: '100px' }} >
+
+    { <ProductContainer/> }
+
+        <div style={{ height: "60%",  width: '100%', backgroundImage: `url(${imageURL})` }}>
+
+        </div>
+
+        <p>
+
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex beatae vitae eum obcaecati nulla aliquid et fugiat ullam sit tempore laborum minima minus facere, possimus tempora voluptates pariatur corrupti voluptatem! Aliquid repudiandae eligendi labore nostrum libero reprehenderit dolor, perspiciatis quaerat molestias illo sint, impedit doloribus! Maxime ea mollitia at distinctio dolorem? Quasi, a totam repellat beatae qui voluptatibus tempora adipisci magni labore consequuntur nulla tenetur unde recusandae possimus numquam animi debitis, architecto natus odit? Aliquid voluptates assumenda incidunt, corporis earum fugiat, dolore, totam neque necessitatibus blanditiis a quibusdam odit reprehenderit!
+        </p>
+
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto fuga, dolorum consectetur praesentium minima itaque magni veniam maiores tempora nostrum delectus voluptates. Ratione repudiandae quisquam consequuntur, adipisci alias odio cumque culpa. Soluta autem vitae, perferendis quisquam doloremque esse fugiat dignissimos vel, consequuntur, maxime velit voluptatem officiis iure a debitis optio placeat rem sunt voluptas est laborum. Aliquam laudantium possimus blanditiis aliquid qui eos at magni nulla nam! Ipsam cupiditate, placeat sunt fugit quisquam saepe inventore laudantium sequi eum dolore adipisci.
+        </p> */
+
+        <div style={{ display: 'block' }}>
+            <Grid container spacing={5} >
+                <Grid item xs={4}>
+                    <ProductCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <ProductCard />
+                </Grid>
+                <Grid item xs={4}>
+                    <ProductCard />
+                </Grid>
+            </Grid>
+        </div>
+
+
+
+
+    // </div>
+
+
+
+/* <Stack>
+
+    <Container sx={{ display: 'flex' }} >
+        
+        
+        
+    </Container>
+
+    
+</Stack> */}
+
+
+    
+
+
+
+    
+
 </>
  )
 }
